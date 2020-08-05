4849b55a11c0209293bedeaf6cbe052ade5117ab
Joexiaohongchen <cxhkobeee@gmail.com>
2020-08-05 14:50:17 -0400

    implemented request-response cycle and save project request on the h2 db

---
+++ src/main/java/com/example/demo/repositories/ProjectRepository.java
@@ -0,0 +1,14 @@
+package com.example.demo.repositories;
+
+import com.example.demo.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+//CrudRepository is a Spring Data interface for generic CRUD operations on a repository of a specific type.
+// It provides several methods out of the box for interacting with a database.
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
+}
---
+++ src/main/java/com/example/demo/web/ProjectController.java
@@ -0,0 +1,34 @@
+package com.example.demo.web;
+
+
+import com.example.demo.domain.Project;
+import com.example.demo.services.ProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//API
+//@RestController is a convenience annotation that is itself annotated with @Controller and @ResponseBody .
+// This annotation is applied to a class to mark it as a request handler. Spring RestController annotation is used to create RESTful web services using Spring MVC.
+@RestController
+//@RequestMapping maps HTTP requests to handler methods of MVC and REST controllers.
+@RequestMapping("api/project")
+public class ProjectController {
+
+    //step1: insert Project object to allow us to create a new project
+    @Autowired
+    private ProjectService projectService;
+
+    //step2: create the route for us to create a new project
+    //@PostMapping return a response entity of type of project
+    @PostMapping("")
+    //ResponseEntity is a type that allows us to have more control on our Json responses and response status.
+    public ResponseEntity<Project> createNewProject(@RequestBody Project project){
+        Project project1 = projectService.saveOrUpdateProject(project);
+        return new ResponseEntity<Project>(project, HttpStatus.CREATED);
+    }
+}
