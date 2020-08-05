46180b178efeab2745af866d2815d65435c4751f
Joexiaohongchen <cxhkobeee@gmail.com>
2020-08-05 16:32:07 -0400

    refactor the validation errors msg, make it reusable to other controllers

--- src/main/java/com/example/demo/web/ProjectController.java
+++ src/main/java/com/example/demo/web/ProjectController.java
@@ -2,6 +2,7 @@ package com.example.demo.web;
 
 
 import com.example.demo.domain.Project;
+import com.example.demo.services.MapValidationError;
 import com.example.demo.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -31,6 +32,9 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private MapValidationError mapValidationErrors;
+
     //step2: create the route for us to create a new project
     //@PostMapping return a response entity of type of project
     @PostMapping("")
@@ -40,14 +44,9 @@ public class ProjectController {
     // The BindingResult must come right after the model object that is validated or else Spring fails to validate the object and throws an exception.
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
 
-        if (result.hasErrors()){
-            Map<String, String> errorMap = new HashMap<>();
-
-            for(FieldError error: result.getFieldErrors()){
-                errorMap.put(error.getField(), error.getDefaultMessage());
-            }
-
-            return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
+        ResponseEntity<?> errorMap = mapValidationErrors.mapValidationError(result);
+        if (errorMap != null){
+            return errorMap;
         }
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
